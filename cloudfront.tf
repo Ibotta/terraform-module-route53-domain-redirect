786702bf488c73142203d7d6868421afda73a330
Mubarak Jama <mubarak.jama@gmail.com>
2022-03-10 14:59:57 -0700

    Can both origin_config be used?

--- cloudfront.tf
+++ cloudfront.tf
@@ -2,7 +2,12 @@ resource "aws_cloudfront_distribution" "redirect" {
   origin {
     domain_name = "${aws_s3_bucket.redirect_bucket.bucket}.s3-website.${data.aws_region.current.name}.amazonaws.com"
     origin_id   = aws_s3_bucket.redirect_bucket.bucket
-
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.redirect.cloudfront_access_identity_path
     }
