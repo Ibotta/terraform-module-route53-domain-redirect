80126c386a649f4d323856aa722ea0502dbb99a7
Mubarak Jama <mubarak.jama@gmail.com>
2022-03-14 12:33:31 -0600

    full s3 public block

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "cloudfront_distribution" {
-    description = "AWS cloudfront distribution"
-    value = aws_cloudfront_distribution.redirect
+  description = "AWS cloudfront distribution"
+  value       = aws_cloudfront_distribution.redirect
 }
 
 output "s3_bucket" {
-    description = "AWS S3 Bucket"
-    value = aws_s3_bucket.redirect_bucket
+  description = "AWS S3 Bucket"
+  value       = aws_s3_bucket.redirect_bucket
 }
 
 output "acm_cert" {
   description = "AWS ACM Certificate"
-  value = aws_acm_certificate.cert
+  value       = aws_acm_certificate.cert
 }
