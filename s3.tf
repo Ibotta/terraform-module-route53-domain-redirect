464cca4f571b5298b5d9b910f3a5ad8b3aa13b67
Mubarak Jama <mubarak.jama@gmail.com>
2022-09-06 14:27:13 -0600

    Standalone S3 acl & website TF resources

--- s3.tf
+++ s3.tf
@@ -5,9 +5,19 @@ resource "random_string" "hash" {
 
 resource "aws_s3_bucket" "redirect_bucket" {
   bucket = "redirect-${local.domain}-${lower(random_string.hash.result)}"
-  acl    = "public-read"
 
-  website {
-    redirect_all_requests_to = var.target_url
+}
+
+resource "aws_s3_bucket_acl" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+  acl = "public-read"
+
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+  redirect_all_requests_to {
+    host_name = var.target_url
   }
+
 }
